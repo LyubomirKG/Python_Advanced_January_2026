cd2ede2c9e447ee4954f192e60a63d238ef7f257
Lyubomir Georgiev <lyubomir.k.georgiev@gmail.com>
2026-01-22 10:36:12 +0200

    my homework

--- Lists_as_Stacks_and_Queues_Exercise/02_stacked_queries.py
+++ Lists_as_Stacks_and_Queues_Exercise/02_stacked_queries.py
@@ -1,3 +1,5 @@
+-----version 1-----
+
 n = int(input())
 my_stack = []
 
@@ -14,3 +16,23 @@ for _ in range(n):
             print(min(my_stack))
 
 print(", ".join(str(x) for x in reversed(my_stack)))
+
+
+----version 2------
+
+
+n = int(input())
+my_stack = []
+
+functions = {
+    "1": lambda x: my_stack.append(int(x)),
+    "2": lambda: my_stack.pop() if my_stack else None,
+    "3": lambda: print(max(my_stack)) if my_stack else None,
+    "4": lambda: print(min(my_stack)) if my_stack else None
+}
+
+for _ in range(n):
+    query = input().split()
+    functions[query[0]](*query[1:])
+
+print(*reversed(my_stack), sep=", ")
